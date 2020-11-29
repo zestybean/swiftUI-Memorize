5ecddca01db99e79934d709a59160e1302ae246c
Brando Lugo <brando.lugo@gmail.com>
2020-11-29 14:01:08 -0600

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
